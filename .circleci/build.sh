f6a5340fd5e2e4f03b0c4ffb546d4b3e71aa9540
Sam Huynh <samhwang21121994@gmail.com>
2019-08-04 14:50:38 +1000

    Add Build number into commit from CircleCI
    Also add the CircleCI Build badge into readme.md

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -9,7 +9,7 @@ mv _site/* .
 rm -R _site/
 
 git add -fA
-git commit --allow-empty -m "$(git log develop -1 --pretty=%B)"
+git commit --allow-empty -m "$CIRCLE_BUILD_NUM"
 git push origin master
 
 echo "Successfully built and deployed."

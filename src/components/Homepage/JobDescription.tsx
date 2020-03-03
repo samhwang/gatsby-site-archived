7e632b3f2f9c0a7778524ddaf72cf7366f3f39b3
Sam Huynh <samhwang2112.dev@gmail.com>
2020-03-04 00:37:32 +1100

    Add technologies involved bit in work experiences

--- src/components/Homepage/JobDescription.tsx
+++ src/components/Homepage/JobDescription.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import LangIcon from './LangIcon';
 
 function JobDescription(props: {
   title: string;
   companyName: string;
-  description: string;
+  description: [string];
   duration: string;
+  techIcons: [string];
+  technologies: [string];
 }) {
-  const { title, companyName, description, duration } = props;
+  const { title, companyName, description, duration, techIcons, technologies } = props;
   return (
     <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
       <div className="resume-content">
         <h3 className="mb-0">{title}</h3>
         <div className="subheading mb-3">{companyName}</div>
-        <p>{description}</p>
+        <p>
+          <ul>
+            {description.map((line, key) => (
+              <li key={key}>{line}</li>
+            ))}
+          </ul>
+        </p>
+        <p>
+          Technologies involved:
+          <ul>
+            {technologies.map((tech, key) => (
+              <li key={key}>{tech}</li>
+            ))}
+          </ul>
+        </p>
+
+        <ul className="list-inline dev-icons">
+          {techIcons.map((icon, key) => (
+            <LangIcon name={icon} key={key} />
+          ))}
+        </ul>
       </div>
       <div className="resume-date text-md-right">
         <span className="text-primary">{duration}</span>
--- src/components/Homepage/information.ts
+++ src/components/Homepage/information.ts
@@ -3,43 +3,95 @@ export const experiences = [
     title: "Junior Full Stack Developer",
     companyName: "Hertz Australia",
     duration: "Oct 2019 - Present",
-    description: "",
-    technologies: [],
+    description: [],
+    techIcons: ['js', 'react', 'node', 'npm', 'yarn', 'php', 'wordpress', 'html5', 'css3', 'sass', 'aws', 'docker', 'github'],
+    technologies: [
+      'PHP 7 and Wordpress',
+      'HTML5, CSS3 and CSS pre-processors like SCSS, SASS and LESS',
+      'GraphQL and REST API',
+      'JavaScript: Node, React',
+      'Docker containers & docker-compose',
+      'MySQL & MongoDB',
+      'Serverless',
+      'AWS Lambda and Elastic Beanstalk',
+      'CI/CD: GitHub Actions pipeline and GitHub packages registry'
+    ],
   },
   {
     title: "Web Application Developer",
     companyName: "Bonntech Business Solutions",
     duration: "May 2019 - Oct 2019",
-    description: "Built the base framework for future development with Bonntech, along with its own create and update script to speed up the initial setup for future projects.",
-    technologies: [],
+    description: [
+      "Built the base framework, along with its own create and update script to speed up the initial setup for future projects.",
+      "Assisted in setting up Bonntech's central Git repository and private Composer Packagist for internal projects and libraries."
+    ],
+    techIcons: ['php', 'js', 'bootstrap', 'css3', 'less', 'sass', 'docker', 'gitlab'],
+    technologies: [
+      "PHP 7.3, using CodeIgniter framework and Composer package manager with self-hosted Packagist",
+      "MySQL database management and data modelling",
+      "HTML5, CSS, Javascript (jQuery, Bootstrap, Datatables, and AJAX)",
+      "Apache server configurations",
+      "Built containerized development environments with Docker",
+    ],
   },
   {
     title: "Software Developer",
     companyName: "Classcom",
     duration: "Jan 2019 - Apr 2019",
-    description: "Collaborated on building new core features of the ClassCom project, in addition to bug fixing, writing automated test and build scripts and participated in code reviews before an update is released in a bi-weekly schedule.",
-    technologies: [],
+    description: [
+      "Collaborated on building new core features of the ClassCom project",
+      "Created automated test and build scripts and participated in code reviews in a bi-weekly release schedule."
+    ],
+    techIcons: ['js', 'node', 'html5', 'css3', 'less', 'npm'],
+    technologies: [
+      "Javascript, using the Meteor JS full stack framework (with NodeJS for backend processing, BlazeJS for templating, CSS pre-processing with LESS)",
+      "Automated test scripting with CucumberJS and Mocha.",
+      "MongoDB database and data modelling.",
+      "REST APIs"
+    ],
   },
   {
     title: "PHP Developer Intern",
     companyName: "Psych Press",
     duration: "Jun 2017 - Sep 2017",
-    description: "Collaborated on building three inhouse projects for Psych Press closely with the psychology team on a weekly basis.",
-    technologies: [],
+    description: ["Collaborated on building three inhouse projects for Psych Press closely with the psychology team on a weekly basis."],
+    techIcons: ['php', 'laravel', 'js', 'bootstrap', 'html5', 'css3', 'aws'],
+    technologies: [
+      "PHP 7, using CodeIgniter, Laravel and Moodle framework.",
+      "Javascript (AJAX, jQuery, Bootstrap, D3js and ChartJS) and CSS (MaterializeCSS)",
+      "Microsoft SQL Server Database",
+      "Migration from Microsoft servers to Amazon Web Services EC2 Linux instances",
+    ],
   },
   {
     title: "Web Developer",
     companyName: "Mint n Co. Accessories",
     duration: "Oct 2015 - Feb 2016",
-    description: "Designed and built the e-commerce site for Mint and Co. Accessories from the ground up and provided regular maintenance to ensure 99.9% site availability.",
-    technologies: [],
+    description: [
+      "Designed and built the e-commerce site for Mint and Co. Accessories from the ground up.",
+      "Provided regular maintenance to ensure 99.9% site availability.",
+    ],
+    techIcons: ['php', 'wordpress', 'bootstrap', 'html5', 'css3', 'js', 'paypal'],
+    technologies: [
+      "CPanel hosting control panel for Apache server",
+      "PHP using the Wordpress framework and WooCommerce e-commerce plugin",
+      "MySQL database",
+      "Javascript (jQuery, Bootstrap)",
+      "Paypal Gateway API",
+    ],
   },
   {
     title: "PHP Developer",
     companyName: "Right Left Communication",
     duration: "Sep 2014 - Feb 2015",
-    description: "Developed, supported, monitored and maintained 4 websites for different clients from various industries.",
-    technologies: [],
+    description: ["Developed, supported, monitored and maintained 4 websites for different clients from various industries."],
+    techIcons: ['php', 'joomla', 'bootstrap', 'html5', 'css3', 'js'],
+    technologies: [
+      "CPanel hosting control panel for Apache server",
+      "PHP using the Joomla framework",
+      "Javascript (jQuery, Bootstrap)",
+      "MySQL database",
+    ],
   },
 ];
 
@@ -57,3 +109,7 @@ export const education = [
     duration: "Oct 2012 - Dec 2015",
   },
 ];
+
+export const projects = [];
+
+export const skills = [];

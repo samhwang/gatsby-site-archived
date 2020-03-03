da0336c9d921dfbd523bd8505dba020e94bc3e75
Sam Huynh <samhwang2112.dev@gmail.com>
2020-03-03 23:51:02 +1100

    Split education and experiences in a different file

---
+++ src/components/Homepage/information.ts
@@ -0,0 +1,59 @@
+export const experiences = [
+  {
+    title: "Junior Full Stack Developer",
+    companyName: "Hertz Australia",
+    duration: "Oct 2019 - Present",
+    description: "",
+    technologies: [],
+  },
+  {
+    title: "Web Application Developer",
+    companyName: "Bonntech Business Solutions",
+    duration: "May 2019 - Oct 2019",
+    description: "Built the base framework for future development with Bonntech, along with its own create and update script to speed up the initial setup for future projects.",
+    technologies: [],
+  },
+  {
+    title: "Software Developer",
+    companyName: "Classcom",
+    duration: "Jan 2019 - Apr 2019",
+    description: "Collaborated on building new core features of the ClassCom project, in addition to bug fixing, writing automated test and build scripts and participated in code reviews before an update is released in a bi-weekly schedule.",
+    technologies: [],
+  },
+  {
+    title: "PHP Developer Intern",
+    companyName: "Psych Press",
+    duration: "Jun 2017 - Sep 2017",
+    description: "Collaborated on building three inhouse projects for Psych Press closely with the psychology team on a weekly basis.",
+    technologies: [],
+  },
+  {
+    title: "Web Developer",
+    companyName: "Mint n Co. Accessories",
+    duration: "Oct 2015 - Feb 2016",
+    description: "Designed and built the e-commerce site for Mint and Co. Accessories from the ground up and provided regular maintenance to ensure 99.9% site availability.",
+    technologies: [],
+  },
+  {
+    title: "PHP Developer",
+    companyName: "Right Left Communication",
+    duration: "Sep 2014 - Feb 2015",
+    description: "Developed, supported, monitored and maintained 4 websites for different clients from various industries.",
+    technologies: [],
+  },
+];
+
+export const education = [
+  {
+    institute: "Navitas Professionals",
+    degree: "Professional Year Program in Computer Science",
+    major: "incl. Cert IV in Business",
+    duration: "Oct 2016 - Oct 2017",
+  },
+  {
+    institute: "RMIT University",
+    degree: "Bachelor of Information Technology",
+    major: "",
+    duration: "Oct 2012 - Dec 2015",
+  },
+];

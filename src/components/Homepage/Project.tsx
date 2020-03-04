57604629aab466cd8a6619acc7733a775cf8e6d3
Sam Huynh <samhwang2112.dev@gmail.com>
2020-03-04 20:23:09 +1100

    Add project component

---
+++ src/components/Homepage/Project.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Project(props: { title: string; description: string; url?: string }) {
+  const { title, description, url } = props;
+
+  let titleText = <strong>{title + ':'}</strong>;
+
+  titleText = url ? (
+    <a href={url}>
+      <strong>{titleText}</strong>
+    </a>
+  ) : (
+    titleText
+  );
+
+  return (
+    <li>
+      <i className="fa-li fa fa-tasks text-warning" />
+      <p>
+        {titleText} {description}
+      </p>
+    </li>
+  );
+}
+
+export default Project;

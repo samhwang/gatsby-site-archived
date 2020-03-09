4e013cda198fa9439cf7594260c47af9d28f64e2
Sam Huynh <samhwang2112.dev@gmail.com>
2020-03-09 14:51:14 +1100

    Add test case for Footer component

---
+++ src/components/Template/Footer.test.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Footer from './Footer';
+
+describe('Footer', () => {
+  it('should render without crashing', () => {
+    const tree = renderer.create(<Footer />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});

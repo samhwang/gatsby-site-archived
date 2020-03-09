a4ffbff1a37fd41f66701304e22dc0cc08eb9ca0
Sam Huynh <samhwang2112.dev@gmail.com>
2020-03-09 14:51:14 +1100

    Add test case for Footer component

---
+++ src/components/Template/Footer.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Footer from './Footer';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../../assets/sass/resume.scss';
+
+export default {
+  title: 'Footer',
+  component: Footer,
+};
+
+export const FooterTest = () => <Footer />;

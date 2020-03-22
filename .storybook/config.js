2d4abf353961d948d5ec36aac1fe732ca8461c93
Sam Huynh <samhwang2112.dev@gmail.com>
2020-03-23 09:10:51 +1100

    Add and config storybook for UI testing

---
+++ .storybook/config.js
@@ -0,0 +1,15 @@
+import { configure } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+configure(require.context('../src', true, /\.stories\.(js|tsx)$/), module);
+
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
+};
+
+global.__PATH_PREFIX__ = '';
+
+window.___navigate = pathname => {
+  action('NavigateTo:')(pathname);
+};

74b18f7021a43d18a80e7da50f379aafe28e8e02
Sam Huynh <samhwang2112.dev@gmail.com>
2020-04-09 23:50:20 +1000

    Reformat footer component

--- src/components/Template/Footer.tsx
+++ src/components/Template/Footer.tsx
@@ -2,19 +2,26 @@ import React from 'react';
 import { OutboundLink } from 'gatsby-plugin-google-gtag';
 
 function Footer() {
+  const GitHubLink = (
+    <OutboundLink href="https://github.com/samhwang">Sam Huynh</OutboundLink>
+  );
+
+  const GitHubPages = (
+    <OutboundLink href="https://pages.github.com/">GitHub Pages</OutboundLink>
+  );
+
+  const GatsbyJSLink = (
+    <OutboundLink href="https://www.gatsbyjs.org/">GatsbyJS.</OutboundLink>
+  );
   return (
     <section className="resume-section p-3 p-lg-5 d-flex align-items-center">
       <p>
-        &copy; 2020{' '}
-        <OutboundLink href="https://github.com/samhwang">
-          Sam Huynh
-        </OutboundLink>
-        . Hosted on{' '}
-        <OutboundLink href="https://pages.github.com/">
-          GitHub Pages
-        </OutboundLink>
-        . Based on{' '}
-        <OutboundLink href="https://www.gatsbyjs.org/">GatsbyJS.</OutboundLink>
+        {`© 2020 `}
+        {GitHubLink}
+        {`. Hosted on `}
+        {GitHubPages}
+        {`. Based on `}
+        {GatsbyJSLink}
       </p>
     </section>
   );

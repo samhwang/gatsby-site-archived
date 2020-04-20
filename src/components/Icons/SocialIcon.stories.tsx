bcec60ca57275b9c9987bf36ab977f478976dbb5
Sam Huynh <samhwang2112.dev@gmail.com>
2020-04-20 16:47:18 +1000

    Add Icon storybook

---
+++ src/components/Icons/SocialIcon.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './index';
+import SocialIcon from './SocialIcon';
+import '../../assets/sass/resume.scss';
+
+export default {
+  title: 'Social Icons',
+  component: SocialIcon,
+};
+
+function IconBase({
+  icon,
+  name,
+  url,
+}: {
+  icon: string;
+  name: string;
+  url: string;
+}) {
+  return (
+    <div className="social-icons">
+      <SocialIcon social={{ icon, name, url }} />
+    </div>
+  );
+}
+
+export const Linkedin = () =>
+  IconBase({
+    icon: 'fab linkedin',
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/samhwang2112/',
+  });
+
+export const Github = () =>
+  IconBase({
+    icon: 'fab github',
+    name: 'GitHub',
+    url: 'https://github.com/samhwang',
+  });

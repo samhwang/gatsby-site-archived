27f0616b0fd0b1f50b3ade24ece55a071813bb88
Sam Huynh <samhwang2112.dev@gmail.com>
2020-04-20 16:47:18 +1000

    Add Icon storybook

---
+++ src/components/Icons/LangIcons.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './index';
+import LangIcon from './LangIcon';
+import '../../assets/sass/resume.scss';
+
+export default {
+  title: 'Language Icons',
+  component: LangIcon,
+};
+
+function IconBase(iconName: string) {
+  return (
+    <ul className="list-inline dev-icons">
+      <LangIcon name={iconName} />
+    </ul>
+  );
+}
+
+export const PHPIcon = () => IconBase('php');
+
+export const JSIcon = () => IconBase('js');

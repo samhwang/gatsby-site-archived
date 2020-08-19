f93835d348ef74233d848ca26307b53e5a136c37
Sam Huynh <samhwang2112.dev@gmail.com>
2020-08-19 15:16:11 +1000

    Rename config to preview to prevent conflict in loading storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js', '../src/**/*.stories.tsx'],
+  stories: ['../src/**/*.stories.js', '../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 };

ea602137db61bfc56e5ecff9708b142837364b9b
Sam Huynh <samhwang2112.dev@gmail.com>
2020-11-06 17:53:12 +1100

    Add static folder env variable into build step

--- .github/workflows/build.bash
+++ .github/workflows/build.bash
@@ -3,13 +3,15 @@
 git fetch origin ${DEPLOY_BRANCH}
 git checkout ${DEPLOY_BRANCH}
 
-find . -maxdepth 1 ! -name 'public' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
-mv public/* .
-rm -Rf public/
+find . -maxdepth 1 ! -name "${STATIC_FOLDER}" ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv ${STATIC_FOLDER}/* .
+rm -Rf ${STATIC_FOLDER}
+
+test -f ".nojekyll" || touch .nojekyll
 
 REMOTE_REPO="https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git add -fA
 git commit --allow-empty -m "Updates"
-git push ${REMOTE_REPO} master
+git push ${REMOTE_REPO} ${DEPLOY_BRANCH}
 
 echo "Successfully built and deployed."

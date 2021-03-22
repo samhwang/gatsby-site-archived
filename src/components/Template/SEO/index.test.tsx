44948d0d8c7b577322d8c50b586afe446ae3c933
Sam Huynh <samhwang2112.dev@gmail.com>
2021-03-22 22:16:34 +1100

    Add SEO Component

---
+++ src/components/Template/SEO/index.test.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { render } from '@testing-library/react';
+import SEO from '.';
+import type { SEOProps } from '.';
+
+describe('SEO Rendering', () => {
+  it('Should match snapshot', () => {
+    const data: SEOProps = {
+      title: 'Test Render',
+      description: 'Test Render',
+      defaultFallback: {
+        title: 'Default Title',
+        description: 'Default Description',
+      },
+    };
+    const SEOComponent = (
+      <SEO
+        title={data.title}
+        description={data.description}
+        defaultFallback={data.defaultFallback}
+      />
+    );
+    const tree = render(SEOComponent);
+    expect(tree).toMatchSnapshot();
+  });
+});

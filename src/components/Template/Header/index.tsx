b4896cc5e0c942c9931452f3b9b7bbde64c37425
Sam Huynh <samhwang2112.dev@gmail.com>
2021-04-19 22:35:46 +1000

    Add styling to header component

--- src/components/Template/Header/index.tsx
+++ src/components/Template/Header/index.tsx
@@ -1,10 +1,59 @@
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { useState } from 'react';
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  createStyles,
+  makeStyles,
+} from '@material-ui/core';
+import type { Theme } from '@material-ui/core';
+import {
+  Menu as MenuIcon,
+  Brightness7 as SunIcon,
+  Brightness4 as MoonIcon,
+} from '@material-ui/icons';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
 
 function Header() {
+  const classes = useStyles();
+  const [showDarkTheme, setShowDarkTheme] = useState(false);
+  const toggleDarkTheme = () =>
+    setShowDarkTheme((currentState) => !currentState);
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6">Sam Huynh</Typography>
+        <IconButton
+          className={classes.menuButton}
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography className={classes.title} variant="h6">
+          Sam Huynh
+        </Typography>
+        <IconButton
+          className={classes.menuButton}
+          onClick={toggleDarkTheme}
+          edge="end"
+          color="inherit"
+          aria-label="dark theme toggle"
+        >
+          {showDarkTheme ? <MoonIcon /> : <SunIcon />}
+        </IconButton>
       </Toolbar>
     </AppBar>
   );

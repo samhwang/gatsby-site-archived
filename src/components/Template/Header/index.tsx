1431fa4d3ed0c0e06933137864c969433ef3dada
Sam Huynh <samhwang2112.dev@gmail.com>
2021-04-19 22:45:59 +1000

    Hide app bar while scrolling

--- src/components/Template/Header/index.tsx
+++ src/components/Template/Header/index.tsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import {
   AppBar,
   IconButton,
+  Slide,
   Toolbar,
   Typography,
+  useScrollTrigger,
   createStyles,
   makeStyles,
 } from '@material-ui/core';
@@ -27,35 +29,38 @@ const useStyles = makeStyles((theme: Theme) =>
 
 function Header() {
   const classes = useStyles();
+  const trigger = useScrollTrigger();
   const [showDarkTheme, setShowDarkTheme] = useState(false);
   const toggleDarkTheme = () =>
     setShowDarkTheme((currentState) => !currentState);
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          className={classes.menuButton}
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography className={classes.title} variant="h6">
-          Sam Huynh
-        </Typography>
-        <IconButton
-          className={classes.menuButton}
-          onClick={toggleDarkTheme}
-          edge="end"
-          color="inherit"
-          aria-label="dark theme toggle"
-        >
-          {showDarkTheme ? <MoonIcon /> : <SunIcon />}
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <Slide appear={false} direction="down" in={!trigger}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6">
+            Sam Huynh
+          </Typography>
+          <IconButton
+            className={classes.menuButton}
+            onClick={toggleDarkTheme}
+            edge="end"
+            color="inherit"
+            aria-label="dark theme toggle"
+          >
+            {showDarkTheme ? <MoonIcon /> : <SunIcon />}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Slide>
   );
 }
 

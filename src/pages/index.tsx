f4fc7d1e271f6b96abd1b5a90210a020e3ae8a4a
Sam Huynh <samhwang2112.dev@gmail.com>
2021-04-20 16:03:27 +1000

    Add index page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,46 @@
 import type { FC } from 'react';
-import { Grid, Typography } from '@material-ui/core';
+// import { OutboundLink } from 'gatsby-plugin-google-gtag';
+import { Grid, Typography, Divider } from '@material-ui/core';
 import { Layout } from '../components/Template';
+import { usePersonalInformationData } from '../hooks';
+import SocialIcon from '../components/Icons/SocialIcon';
+import type { SocialLinkProps } from '../components/Icons/SocialIcon';
 
-const IndexPage: FC = () => (
-  <Layout title="Sam Huynh" description="Sam Huynh Home Page">
-    <Grid container>
-      <Typography paragraph variant="h1">
-        Hi there.
-      </Typography>
-      <Typography paragraph variant="body1">
-        {/* This is just lorem ipsum */}
-        Well, the way they make shows is, they make one show. That show&apos;s
-        called a pilot. Then they show that show to the people who make shows,
-        and on the strength of that one show they decide if they&apos;re going
-        to make more shows. Some pilots get picked and become television
-        programs. Some don&apos;t, become nothing. She starred in one of the
-        ones that became nothing.
-      </Typography>
-    </Grid>
-  </Layout>
-);
+const IndexPage: FC = () => {
+  const { firstName, lastName, address, email, socialLinks } =
+    usePersonalInformationData();
+  const fullName = `${firstName} ${lastName}`;
+  const socialLinksData: SocialLinkProps[] = [
+    ...socialLinks,
+    { name: 'email', url: `mailto:${email}` },
+  ];
+
+  return (
+    <Layout title="Sam Huynh" description="Sam Huynh Home Page">
+      <Grid item container md={12} alignContent="center">
+        <Typography paragraph variant="h1">
+          What&apos;s up?
+        </Typography>
+        <Typography paragraph variant="h5">
+          My name is {`${fullName}`}. I am a Full Stack Software Developer in
+          {` ${address}`} and passionate about all things tech and modern
+          software development.
+        </Typography>
+        <Typography paragraph variant="h5">
+          I am always looking for new opportunities. Wanna reach me? Just give
+          me a shout!
+        </Typography>
+        <Divider />
+      </Grid>
+      <Grid item container md={12} alignContent="center" justify="center">
+        {socialLinksData.map(({ name, url }) => (
+          <Grid key={name} item md>
+            <SocialIcon name={name} url={url} />
+          </Grid>
+        ))}
+      </Grid>
+    </Layout>
+  );
+};
 
 export default IndexPage;

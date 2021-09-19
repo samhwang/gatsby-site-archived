41ad8b03a0c3d33101a4fb518b6ccc5f04e1c64c
Sam Huynh <samhwang2112.dev@gmail.com>
2021-09-19 21:50:31 +1000

    Remove Resume file from the repo

--- config.js
+++ config.js
@@ -25,11 +25,6 @@ module.exports = {
         name: 'Linkedin',
         url: 'https://www.linkedin.com/in/samhwang2112/',
       },
-      {
-        icon: 'far file-pdf',
-        name: 'PDF',
-        url: '/files/Resume_SamHuynh.pdf',
-      },
     ],
     email: 'samhwang2112.dev@gmail.com',
     address: 'Melbourne, Victoria, Australia',

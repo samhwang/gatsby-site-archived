3d1670d9496c38b670c5545acb41e92640c06baf
Sam Huynh <samhwang2112.dev@gmail.com>
2022-04-18 10:45:48 +1000

    Fix eslint errors

--- src/components/Icons/useIconClass.ts
+++ src/components/Icons/useIconClass.ts
@@ -2,13 +2,13 @@ import { useMemo, useCallback } from 'react';
 import { useToggle } from 'rooks';
 
 const useIconClass = (name: string) => {
-  const DEFAULT_ICON_CLASS = useMemo<string>(() => `devicon-${name}`, []);
+  const DEFAULT_ICON_CLASS = useMemo<string>(() => `devicon-${name}`, [name]);
   const toggleColorIcon = useCallback(
-    (currentClass) =>
+    (currentClass: string) =>
       currentClass === DEFAULT_ICON_CLASS
         ? `${DEFAULT_ICON_CLASS} colored`
         : DEFAULT_ICON_CLASS,
-    []
+    [DEFAULT_ICON_CLASS]
   );
 
   const [currentIconClass, toggleIconClass] = useToggle(

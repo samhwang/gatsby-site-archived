cc675a903b4be776a068e3474464e66771f0bf13
Sam Huynh <samhwang2112.dev@gmail.com>
2022-04-18 10:23:39 +1000

    Remove unused path prefix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 const config = require('./config');
 
 module.exports = {
-  pathPrefix: config.pathPrefix,
   siteMetadata: {
     title: config.siteTitle,
     description: config.siteDescription,
